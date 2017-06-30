fcde5402c3e576e79ca70fac554e2d71a3bdd2df
Justin Gomez <programjustin@gmail.com>
2017-06-30 12:54:37 -0700

    append list of doctors based on users input

--- js/main-interface.js
+++ js/main-interface.js
@@ -1,6 +1,8 @@
 var apiKey = require('./../.env').apiKey;
 
 
+
+
 $(document).ready(function() {
 
   $('form').submit(function(event) {
@@ -11,19 +13,16 @@ $(document).ready(function() {
 var medicalIssue = $('#issue').val();
 alert(medicalIssue);
 
-$.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey, function(response) {
-  $('#showDoctor').append("Your Doctor" + " is " + response.data[0].practices[0].name);
-  console.log(response);
-  console.log(response.data[0].practices[0].name);
+$.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=5&user_key=' + apiKey, function(response) {
+
+  for(var i = 0; i < 5; i++) {
+  $('#showDoctor').append("<li>" + response.data[i].practices[0].name + "</li>");
+}
+   console.log(response);
+   console.log(response.data[0].practices[0].name);
 
 
-
-
-
-  });
-
-
-
+    });
 
   });
 
