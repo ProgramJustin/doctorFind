31ce2e86ec4d97820cb25866f01afac0045f79c2
Justin Gomez <programjustin@gmail.com>
2017-06-30 13:29:30 -0700

    add container and header, add hero imahe to header

--- js/main-interface.js
+++ js/main-interface.js
@@ -8,10 +8,8 @@ $(document).ready(function() {
   $('form').submit(function(event) {
     event.preventDefault();
 
-  var city = "Portland";
-
 var medicalIssue = $('#issue').val();
-alert(medicalIssue);
+
 
 $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=5&user_key=' + apiKey, function(response) {
 
