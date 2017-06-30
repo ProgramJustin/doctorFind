9917db454ac1d0abbf4a025fffc546d0f014b405
Justin Gomez <programjustin@gmail.com>
2017-06-30 11:11:18 -0700

    append name of specialty doctor to the page

--- js/main-interface.js
+++ js/main-interface.js
@@ -3,23 +3,27 @@ var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
 
-  $('button').click(function() {
+  $('form').submit(function(event) {
+    event.preventDefault();
+
   var city = "Portland";
 
+var medicalIssue = $('#issue').val();
+alert(medicalIssue);
+
+$.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey, function(response) {
+  $('#showDoctor').append("Your Doctor" + " is " + response.data[0].practices[0].name);
+  console.log(response);
+  console.log(response.data[0].practices[0].name);
 
 
 
-var medicalIssue = "headache";
 
 
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue[0]+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey, function(response) {
-    $('#showDoctor').text("Your Doctor" + " is " + response);
-    console.log(response);
-
-    alert("hi");
+  });
 
 
-    });
+
 
   });
 
