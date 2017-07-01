70ac0c01a7a9baa1c24d656a5153a9148b28acd5
Justin Gomez <programjustin@gmail.com>
2017-07-01 13:18:59 -0700

    remove console.log and alerts

--- js/backend.js
+++ js/backend.js
@@ -8,15 +8,11 @@ Finder.prototype.find = function(issues, key) {
   alert("test");
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ issues + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=5&user_key=' + key, function(response) {
 
-    console.log(response);
-
     for(var i = 0; i < 5; i++) {
     $('#showDoctor').append("<a>" + response.data[i].practices[0].name + '<img src="' + response.data[i].profile.image_url + '" alt=""/>' + "</a>");
-  }
-     console.log(response.data[0].practices[0].name);
+    }
 
-
-      });
+  });
 
 };
 
