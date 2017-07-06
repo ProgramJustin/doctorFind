5a849e143af489a3f2bfbc4829c89ef059010bbe
Justin Gomez <programjustin@gmail.com>
2017-07-05 23:20:34 -0700

    seperate front end and back end logic

--- js/backend.js
+++ js/backend.js
@@ -1,18 +1,15 @@
 
-function Finder(name) {
-  this.name = name;
+function Finder() {
 }
 
-Finder.prototype.find = function(issues, key) {
+Finder.prototype.find = function(issues, key, sendToWindow) {
 
-  alert("test");
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ issues + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=5&user_key=' + key, function(response) {
+   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ issues + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=5&user_key=' + key).then(function(response) {
 
-    for(var i = 0; i < 5; i++) {
-    $('#showDoctor').append("<a>" + response.data[i].practices[0].name + '<img src="' + response.data[i].profile.image_url + '" alt=""/>' + "</a>");
-    }
+    sendToWindow(response);
 
   });
+
 
 };
 
--- js/main-interface.js
+++ js/main-interface.js
@@ -1,22 +1,23 @@
 var apiKey = require('./../.env').apiKey;
-
 var Finder = require('../js/backend.js').doctorModule;
 
+var sendDoctors = function(dataToSend) {
 
+  console.log(dataToSend);
 
+  for(var i = 0; i < 5; i++) {
+    $('#showDoctor').append("<a>" + dataToSend.data[i].practices[0].name + '<img src="' + dataToSend.data[i].profile.image_url + '" alt=""/>' + "</a>");
+  }
 
+};
 
 $(document).ready(function() {
-
-
   $('form').submit(function(event) {
     event.preventDefault();
-
     var medicalIssue = $('#issue').val();
     var finded = new Finder();
+  finded.find(medicalIssue, apiKey, sendDoctors);
 
-
-    finded.find(medicalIssue, apiKey);
 
   });
 
